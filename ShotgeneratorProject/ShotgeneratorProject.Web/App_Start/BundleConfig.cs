09c9969a74b5a126a8011f5fb21adbd055c1aa61
Ludvig Flemmich <ludvig.flemmich@esatto.se>
2016-08-05 12:59:37 +0200

    Added bundle config and main css file

--- ShotgeneratorProject/ShotgeneratorProject.Web/App_Start/BundleConfig.cs
+++ ShotgeneratorProject/ShotgeneratorProject.Web/App_Start/BundleConfig.cs
@@ -5,8 +5,20 @@ namespace ShotgeneratorProject.Web
 {
     public class BundleConfig
     {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/scripts/").Include(
+               "~/bower_components/angular/angular.js",
+               "~/bower_components/angular-ui-router/release/angular-ui-router.js",
+               "~/bower_components/jquery/dist/jquery.js",
+               "~/bower_components/bootstrap/dist/js/bootstrap.js",
+               "~/scripts/app/app.js"
+               ));
+            bundles.Add(new StyleBundle("~/styles/").Include(
+                "~/bower_components/bootstrap/dist/css/bootstrap.css",
+                "~/styles/main.css"
+                ));
         }
     }
 }
