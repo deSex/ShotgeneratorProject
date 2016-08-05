ea9a6dbd6965f8cf4d8184054095c77cb6ff898d
Ludvig Flemmich <ludvig.flemmich@esatto.se>
2016-08-05 10:51:35 +0200

    Reverted all changes - new fresh project

---
+++ ShotgeneratorProject/ShotgeneratorProject.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShotgeneratorProject.Web.Startup))]
+namespace ShotgeneratorProject.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
