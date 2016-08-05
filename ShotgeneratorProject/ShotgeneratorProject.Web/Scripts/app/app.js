c6f306f80119115ab74fac1fba6db13b9fad3dc3
alexander.helsinghof <alexander.helsinghof@esatto.se>
2016-08-05 13:07:24 +0200

    added app.js and routing

---
+++ ShotgeneratorProject/ShotgeneratorProject.Web/Scripts/app/app.js
@@ -0,0 +1,15 @@
+﻿
+'use strict';
+angular.module('app', ['ngRoute'])
+.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('generator', {
+        templateUrl: 'Views/Generator/generator.cshtml'
+    })
+    .when('home', {
+        templateUrl: 'Views/Home/Index.cshtml'
+    });
+    $locationProvider.html5Mode(true);
+});
+
+
